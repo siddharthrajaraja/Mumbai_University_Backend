8f822072b5cac9bfaee62bd28a933436301f4b75
SIDDHARTH RAMESH CHAND RAJA <45534008+siddharthrajaraja@users.noreply.github.com>
2020-03-03 00:53:03 +0530

    addding new database to cloud

--- keys/keys.js
+++ keys/keys.js
@@ -1,6 +1,6 @@
 module.exports={
     mongodb:{
-        "cloudURL":"mongodb+srv://university:university@cluster0-eakxp.mongodb.net/test?retryWrites=true&w=majority",
+        "cloudURL":"mongodb+srv://root:Lumen@12345@rajacluster-b6xpu.mongodb.net/GYM?retryWrites=true&w=majority",
         "localURL":"mongodb://127.0.0.1:27017/MumbaiUniversity"
     },
     twilio:{
